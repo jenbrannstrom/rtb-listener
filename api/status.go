3b3366ea6556999e939a5458cd29a6204e63d1cc
Reopard226 <robcoder@hotmail.com>
2020-09-16 10:22:06 -0400

    minor change

--- api/status.go
+++ api/status.go
@@ -1,20 +1,12 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
-	"google-rtb/model"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 // StatusCheck tests health
 func StatusCheck(c *gin.Context) {
-	var requestBody model.RequestBody
-
-	b, _ := json.Marshal(requestBody)
-	fmt.Println("request body", time.Now().String(), string(b))
 	c.JSON(http.StatusOK, map[string]string{"status": "ok"})
 }
