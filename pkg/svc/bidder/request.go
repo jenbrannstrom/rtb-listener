1e3c0f4c40e60f119eed0150dff08e03c5fef417
robcoder26 <robcoder@hotmail.com>
2020-11-06 02:50:25 +1000

    remove response loggin

--- pkg/svc/bidder/request.go
+++ pkg/svc/bidder/request.go
@@ -33,10 +33,6 @@ func SendBidRequest(url string, requestBody model.RequestBody) *model.RequestBod
 	}
 
 	if len(body) > 0 {
-		params := &logger.LogParams{}
-		params.Add("response:", string(body))
-		logger.ErrorP("getting response", params)
-
 		_ = json.Unmarshal(body, &res)
 		// go streamer.ProcessRequestBody(res)
 		return res
